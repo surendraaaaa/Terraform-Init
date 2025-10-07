12246a4e1645ff81238d13329dc32d44addaa75d
surendraaaaa <ashukumavat555@.com>
2025-10-07 16:58:47 +0000

    added ec2 provisioning using variables

---
+++ aws-infra-with-terraform/ec2_variable/ec2.tf
@@ -0,0 +1,80 @@
+# key pair
+
+resource aws_key_pair "my_key" {
+
+	key_name = "aws_kp"
+	public_key = file("aws_kp.pub")
+
+	}
+
+# VPC
+
+resource aws_default_vpc "my_vpc"{
+
+	}
+
+# security groups
+
+resource aws_security_group "my_security_group" {
+
+	 name        = var.security_group_name
+ 	 description = "Allow TLS inbound traffic and all outbound traffic"
+  	 vpc_id      = aws_default_vpc.my_vpc.id
+
+  	 tags = {
+    		Name = var.security_group_name
+		}
+	}
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4_https" {
+	  security_group_id = aws_security_group.my_security_group.id
+	  cidr_ipv4         = "0.0.0.0/0"
+	  from_port         = 443
+	  ip_protocol       = "tcp"
+	  to_port           = 443
+	  description	    = "https port"
+	}	
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4_http" {
+          security_group_id = aws_security_group.my_security_group.id
+          cidr_ipv4         = "0.0.0.0/0"
+          from_port         = 80
+          ip_protocol       = "tcp"
+          to_port           = 80
+          description       = "HTTP port"
+        }
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4_ssh" {
+          security_group_id = aws_security_group.my_security_group.id
+          cidr_ipv4         = "0.0.0.0/0"
+          from_port         = 22
+          ip_protocol       = "tcp"
+          to_port           = 22
+	  description	    = "SSH port"
+        }
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+		security_group_id = aws_security_group.my_security_group.id
+		from_port = 0
+		to_port = 0
+		ip_protocol = "-1"
+		cidr_ipv4 = "0.0.0.0/0"
+		description = "All access"
+	}
+
+
+# ec2 instance
+
+resource aws_instance "my_instance" {
+
+	ami = var.instance_ami_id #ubuntu
+	instance_type = var.instance_type
+	key_name = aws_key_pair.my_key.key_name
+	security_groups = [aws_security_group.my_security_group.name]
+	
+	root_block_device {
+		volume_size = var.instance_block_storage
+		volume_type = "gp3"
+		}
+	tags = {
+		Name = var.instance_name
+	}
+}
